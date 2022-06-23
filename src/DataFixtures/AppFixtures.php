f31f018bf74a1f7b2c0e24525cab4d2f6f86577f
feraudTino <feraud.tino@gmail.com>
2022-06-23 11:05:16 +0200

    Ajout entité film

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,34 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Films;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $film = new Films();
+        $film->setNom('The Shawshank Redemption');
+        $film->setDescription('Two imprisoned');
+        $film->setGenre('Drama');
+        $film->setAnnee(new \DateTime('1994-09-23'));
+        $manager->persist($film);
+
+        $film = new Films();
+        $film->setNom('The Godfather');
+        $film->setDescription('Two imprisoned');
+        $film->setGenre('Drama');
+        $film->setAnnee(new \DateTime('1994-09-23'));
+        $manager->persist($film);
+
+        $film = new Films();
+        $film->setNom('The Godfather: Part II');
+        $film->setDescription('Two imprisoned');
+        $film->setGenre('Drama');
+        $film->setAnnee(new \DateTime('1994-09-23'));
+        $manager->persist($film);
 
         $manager->flush();
     }
---
+++ src/Entity/Films.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\FilmsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FilmsRepository::class)]
+#[ApiResource]
+class Films
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $nom;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $genre;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private $annee;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(?string $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getAnnee(): ?\DateTimeInterface
+    {
+        return $this->annee;
+    }
+
+    public function setAnnee(?\DateTimeInterface $annee): self
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+}
